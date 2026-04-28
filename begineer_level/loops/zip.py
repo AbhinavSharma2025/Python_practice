40d05361d969944a72657dd2d957e2e63ae286e1
Abhinav Sharma <abhinav90682@gmail.com>
2026-04-28 15:19:50 +0530

    output greetings

--- begineer_level/loops/zip.py
+++ begineer_level/loops/zip.py
@@ -1,5 +1,6 @@
 names=["a","b","c","d","e"]
 bill= [30,67,34,23,56]
 for nam,cos in zip(names,bill):
-    print(f"{nam} paid {cos} rupees please verify")
+    print(f"{nam} paid {cos} rupees please verify.Come to next line")
+    print(f"Thank you {nam} for shopping with us today. Have a nice day!")
 print("I am doing well what about you")
