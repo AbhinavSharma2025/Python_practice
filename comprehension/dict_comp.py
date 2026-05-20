33c683f445552dbbd63a61c966f8fa3448e7d8d1
Abhinav Sharma <abhinav90682@gmail.com>
2026-05-21 02:25:58 +0530

    Delete comprehension directory

--- comprehension/dict_comp.py
+++
@@ -1,9 +0,0 @@
-#{expression (key:value) for item in iterable if condition}
-tea_prices_inr={
-    "Masala Chai":40,
-    "Green Tea":50,
-    "Lemon Tea":200
-}
-
-tea_prices_usd={tea:round(price/95,3) for tea,price in tea_prices_inr.items()}
-print(tea_prices_usd)
--- comprehension/generator_comp.py
+++
@@ -1,6 +0,0 @@
-#to save memory
-#(expression for item in iterable if condition) one iteam at a time like a stream
-
-daily_sales=[5,10,12,7,3,8,9,15]
-total_cups=sum(sale for sale in daily_sales if sale>5)
-print(total_cups)
